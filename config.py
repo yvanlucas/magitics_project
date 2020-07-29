1b5a62dd8f110a022fdf885b9224aeffcabd8b42
Yvan Lucas <ylucas@bb8-2.cluster>
2020-07-29 10:23:06 +0200

    changes remote

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # PATHs
-id='2'
+id='3'
 xp_name = 'pseud_levo_31'
 
 data = 'Pseudomonas_aeruginosa/levofloxacin/'
@@ -12,12 +12,12 @@ len_kmers = 31  # If len_kmers<=8, gerbil will set len_kmers to 8
 min_abundance = 3
 
 # Learning parameters
-model = 'gradient'  # can be ['rf','SCM', 'gradient']
+model = 'SCM'  # can be ['rf','SCM', 'gradient']
 
 rf_grid = {'max_features': ['sqrt', 'log2'],
            'max_depth': [4, 8]}
 
-SCM_grid = {'model_type': ['disjunction'], 'max_rules': [10, 100]}
+SCM_grid = {'model_type': ['disjunction', 'conjunction'], 'max_rules': [3 ,10, 100]}
 
 gradient_grid = {'max_depth': [1, 5],
-                 'n_estimators': [30, 50, 100]}
+                 'n_estimators': [10, 40, 70]}
